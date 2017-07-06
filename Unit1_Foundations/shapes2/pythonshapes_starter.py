eed6515ad4922bffa5869590650fecb64f2f9ab1
sarahnpadilla <sarahpadilla101@gmail.com>
2017-07-06 12:40:05 -0700

    Update pythonshapes_starter.py

--- Unit1_Foundations/shapes2/pythonshapes_starter.py
+++ Unit1_Foundations/shapes2/pythonshapes_starter.py
@@ -6,16 +6,16 @@ t = Turtle()
 
 # Set Up your screen and starting position.
 setup(500,300)
-x_pos = -250
-y_pos = -150
-t.setposition(x_pos, y_pos)
-
 ### Write your code below:
-
-
-
-
-
+begin_fill()
+pencolor("red")
+fillcolor("green")
+numsides=8
+angle=360/numsides
+for hops in range(numsides):
+    forward(50)
+    right(angle)
+end_fill()
 
 # Close window on click.
 exitonclick()
